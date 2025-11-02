25d711ed0dc30f0488af2bf43186265e2dc6979a
Maurim <m_webhost@yahoo.com.br>
2025-11-01 21:39:20 -0300

    setup project

---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { Application } from 'express';
+//import { authRoutes } from '@auth/routes/authRoutes';
+//import { serverAdapter } from '@service/queues/base.queue';
+//import { currentUserRoutes } from '@auth/routes/currentRoutes';
+//import { authMiddleware } from '@global/helpers/auth-middleware';
+//import { postRoutes } from '@post/routes/postRoutes';
+//import { reactionRoutes } from '@reaction/routes/reactionRoutes';
+//import { commentRoutes } from '@comment/routes/commentRoutes';
+//import { followerRoutes } from '@follower/routes/followerRoutes';
+//import { notificationRoutes } from '@notification/routes/notificationRoutes';
+//import { imageRoutes } from '@image/routes/imageRoutes';
+//import { chatRoutes } from '@chat/routes/chatRoutes';
+//import { userRoutes } from '@user/routes/userRoutes';
+//import { healthRoutes } from '@user/routes/healthRoutes';
+
+const BASE_PATH = '/api/v1';
+
+export default (app: Application) => {
+  const routes = () => {};
+  routes();
+};
